d6d2cb9dab9614dacf054eb1fa01d4fd22a4ac86
Bejan Corneliu <bejancorneliu@gmail.com>
2018-04-17 15:24:42 +0300

    blue-4

--- app/src/main/java/com/personalstyler/client/testarelib/MainActivity.java
+++ app/src/main/java/com/personalstyler/client/testarelib/MainActivity.java
@@ -2,7 +2,6 @@ package com.personalstyler.client.testarelib;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.personalstyler.client.mylibrary.TestA;
 
 
 public class MainActivity extends AppCompatActivity {
